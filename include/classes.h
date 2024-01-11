233eee7de647167f44bdf9d087105beb4e97bd0d
spykelionel <social.lionel@gmail.com>
2024-01-11 20:12:11 +0100

    create forward declarations for classes

---
+++ include/classes.h
@@ -0,0 +1,14 @@
+#ifndef CLASSES_H_INCLUDED
+#define CLASSES_H_INCLUDED
+
+// Forward declarations
+class Game;
+class Map;
+class Character;
+class Player;
+class Monster;
+class Room;
+class PoisonRoom;
+class TrapRoom;
+
+#endif // CLASSES_H_INCLUDED
